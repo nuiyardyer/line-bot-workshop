fa159d94ef7b907c70746d58705030f7df3d9008
nuiyardyer <46663621+nuiyardyer@users.noreply.github.com>
2019-01-14 11:29:33 +0700

    update

--- Desktop/line-bot-unlimit_friend-master/profile.php
+++ Desktop/line-bot-unlimit_friend-master/profile.php
@@ -1,9 +1,9 @@
 <?php
 
 
-$access_token = '*Channel_Access_Token*';
+$access_token = 'R1jBvQtCkQTBwiVS+ECHJ49ol8lzRuQYWyXzMgFDs4/+BkydsBp1+244awYuTiWloDD6L/jlxiMQHz2v/t6apn3BIxbxpzjPWg8HdNLi784/HV0zcj7IHloWmE969jmdV9+3h39wKJUXYvF4TTmzNgdB04t89/1O/w1cDnyilFU=';
 
-$userId = '*User_ID*';
+$userId = 'U406d122cc1027e9547e36045abdf1c31';
 
 $url = 'https://api.line.me/v2/bot/profile/'.$userId;
 
